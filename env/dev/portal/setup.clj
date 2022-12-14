3f3d747691b507b6b02f78f95476ca88e72fe950
Peter Strömberg <pez@pezius.com>
2022-12-14 16:45:43 +0100

    Default to tree tapping

--- env/dev/portal/setup.clj
+++ env/dev/portal/setup.clj
@@ -1,12 +1,26 @@
 (ns portal.setup
   (:require [portal.api :as portal]))
 
-(def !state (atom {:portal nil}))
+(def !state (atom {:p nil}))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn start! []
-  (swap! !state assoc :portal (portal/open (merge {:launcher :vs-code})))
-  (add-tap #'portal/submit))
+(defn can-meta? [value]
+  (instance? clojure.lang.IObj value))
+
+(defn submit [value]
+  (portal/submit
+   (cond
+     (:portal.nrepl/eval (meta value)) value
+     (can-meta? value) (with-meta value {:portal.viewer/default :portal.viewer/tree})
+     :else value)))
+
+(defn start! [extra-config]
+  (swap! !state assoc :p (portal/open (merge {}
+                                             extra-config)))
+  (add-tap #'submit))
 
 (comment
-  (tap> ::foo))
+  (start! {:launcher :vs-code})
+  (add-tap #'portal/submit)
+  (tap> {:launcher :vs-code})
+  (tap> :foo)
+  )
